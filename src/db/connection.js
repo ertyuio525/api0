02a1bda63a938ccf9c4a0a98c6003ad0fa6bc427
ertyuio525 <abanoub567643@gmail.com>
2026-05-06 19:06:40 +0300

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,16 +1,5 @@
-const mongoose = require('mongoose');
-
 const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://bruxo:eg270209@bruxo.ztkst.mongodb.net/SiteDB?retryWrites=true&w=majority&appName=bruxo', {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-        });
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Error connecting to MongoDB', err);
-        process.exit(1);
-    }
+    console.log("MongoDB disabled 🚫 (running without database)");
 };
 
 module.exports = connectDB;
