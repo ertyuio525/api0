be2824927835a72a0597e2db62a7844f8467c80f
ertyuio525 <abanoub567643@gmail.com>
2026-05-06 19:05:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const path = require('path');
-
+const connectDB = require('./db/connection.js');
 
 const setupRoutes = require('./server/routes'); // تأكد إنه بيرجع router
 
